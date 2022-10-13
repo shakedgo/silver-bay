f80955d4cd620fa265ce1f69df7e0fc3bca6160d
student <shakedgo@gmail.com>
2022-10-13 15:50:15 +0300

    Added scraper template

---
+++ server/Scraper.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGO;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+const URL = "http://scrapesite.com"; // site we scrape
+
+const scrape = () => {
+	// Just a demo for cheerio
+	// =========================================================================
+	// const $ = cheerio.load(page.data);
+	// $(".first").each((i, element) => {
+	// 	title.push($(element).text().trimStart().trimEnd());
+	// 	ids.push($(element).find("a").attr("href").split("/")[4]);
+	// 	$(element)
+	// 		.siblings("td")
+	// 		.each((siblingIndex, sibling) => {
+	// 			if (siblingIndex === 0) author.push($(sibling).text());
+	// 			if (siblingIndex === 2) {
+	// 				date.push(chrono.parseDate($(sibling).text()).toUTCString());
+	// 			}
+	// 		});
+	// });
+};
+module.exports = { scrape };
