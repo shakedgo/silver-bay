d3eebd9b7d4993157f64f1d4804f922757113ab7
student <shakedgo@gmail.com>
2022-10-13 15:33:24 +0300

    Added NavBar

---
+++ client/src/Navigation/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+export default function Navbar() {
+	return (
+		<div className="container">
+			<Link to={"/best"}>Best Seller</Link>
+			<Link to={"/gold"}>Gold</Link>
+			<Link to={"/silver"}>Silver</Link>
+			<Link to={"/platpald"}>Platinum & Palladium</Link>
+			<Link to={"/deals"}>Deals</Link>
+			<Link to={"/rare"}>Rare Coins</Link>
+			<Link to={"/curr"}>Currency & Others</Link>
+			<Link to={"/sell"}>Sell to Us</Link>
+			<Link to={"/charts"}>Charts</Link>
+			<Link to={"/resources"}>Resources</Link>
+		</div>
+	);
+}
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navigation/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<App />}></Route>
 			</Routes>
