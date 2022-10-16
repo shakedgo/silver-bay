4b51016d95358be197cc993d380475341d463987
student <shakedgo@gmail.com>
2022-10-16 22:32:42 +0300

    Changed sort section

--- client/src/Components/Sort.js
+++ client/src/Components/Sort.js
@@ -20,6 +20,24 @@ export default function Sort() {
 				<input type="checkbox" name="pallad" id="pallad" value="pallad" />
 				<label htmlFor="pallad">Palladium</label>
 			</div>
+			<br />
+			<h4>Metal Type</h4>
+			<div>
+				<input type="checkbox" name="gold" id="gold" value="gold" />
+				<label htmlFor="gold">Gold</label>
+			</div>
+			<div>
+				<input type="checkbox" name="silver" id="silver" value="silver" />
+				<label htmlFor="silver">Silver</label>
+			</div>
+			<div>
+				<input type="checkbox" name="plat" id="plat" value="plat" />
+				<label htmlFor="plat">Platinum</label>
+			</div>
+			<div>
+				<input type="checkbox" name="pallad" id="pallad" value="pallad" />
+				<label htmlFor="pallad">Palladium</label>
+			</div>
 		</div>
 	);
 }
--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,26 +1,29 @@
 import React from "react";
 import Card from "../Components/Card";
 import Sort from "../Components/Sort";
+import "./About.scss";
 
 export default function About() {
 	return (
-		<div className="about" style={{ display: "flex", flexWrap: "wrap" }}>
+		<div className="about">
 			<Sort />
-			<Card /> {/* Every card will be fetched from the database, with the relevant sort */}
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
+			<div className="cards">
+				<Card /> {/* Every card will be fetched from the database, with the relevant sort */}
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
 		</div>
 	);
 }
