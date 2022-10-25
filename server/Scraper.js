9e1275178147ee4b0777c3cb6736d571005fecb1
student <shakedgo@gmail.com>
2022-10-25 13:42:59 +0300

    Starting scrape function

--- server/Scraper.js
+++ server/Scraper.js
@@ -1,24 +1,27 @@
 const axios = require("axios");
+const { INSPECT_MAX_BYTES } = require("buffer");
 const cheerio = require("cheerio");
 
-const URL = "http://scrapesite.com"; // site we scrape
+const URL = "https://www.apmex.com/category/10010/gold-coins?page=3"; // site we scrape
 
-const scrape = () => {
+const scrape = async () => {
 	// Just a demo for cheerio
-	// =========================================================================
-	// const $ = cheerio.load(page.data);
-	// $(".first").each((i, element) => {
-	// 	title.push($(element).text().trimStart().trimEnd());
-	// 	ids.push($(element).find("a").attr("href").split("/")[4]);
-	// 	$(element)
-	// 		.siblings("td")
-	// 		.each((siblingIndex, sibling) => {
-	// 			if (siblingIndex === 0) author.push($(sibling).text());
-	// 			if (siblingIndex === 2) {
-	// 				date.push(chrono.parseDate($(sibling).text()).toUTCString());
-	// 			}
-	// 		});
-	// });
+	// ============================================================
+	let items = [];
+	let page = await axios.get(URL);
+	const $ = cheerio.load(page.data);
+	// console.log($(".mod-product-card").children);
+	$(".mod-product-card").each((i, element) => {
+		console.log("in");
+		let item = new Object();
+		item._id = $(element).text().trimStart().trimEnd();
+		item.title = $(element).text().trimStart().trimEnd();
+		item.img = $(element).text().trimStart().trimEnd();
+		item.price = $(element).text().trimStart().trimEnd();
+		items.push({ ...item });
+	});
+	console.log(items);
 };
+scrape();
 
 module.exports = { scrape };
